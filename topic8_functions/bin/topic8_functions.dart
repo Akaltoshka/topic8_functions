5fae6e58951ec8dd5424e749ff5fd47006ce2f58
Akaltoshka <120437160+Akaltoshka@users.noreply.github.com>
2023-12-04 12:59:24 +0600

    Add files via upload

---
+++ topic8_functions/bin/topic8_functions.dart
@@ -0,0 +1,50 @@
+import 'dart:math';
+
+//task1
+// void main() {
+//   List<int> n = [1, 2, 3];
+//   int max = Function(n);
+//   print("max number is $max");
+// }
+
+// int Function(List n) {
+//   int maxNum = max(max(n[0], n[1]), n[2]);
+//   return maxNum;
+// }
+
+//task2
+// void main() {
+//   print(sum(8, 2, 3, 0, 7)); // 20
+// }
+
+// int sum(int a, int b, int c, int d, int e) => a + b + c + d + e;
+
+//task3
+// void main() {
+//   print(sum(8, 2, 3, -1, 7)); // -336
+// }
+
+// int sum(int a, int b, int c, int d, int e) => a * b * c * d * e;
+
+//task4
+// void main() {
+//   String stringSoz = "1234abcd";
+//   String reversedString = stringSoz.split('').reversed.join();
+//   print(reversedString);
+// }
+
+//task5
+// void main() {
+//   int n = 3;
+//   int formula = formulaFactorial(n);
+
+//   print("Factorial of $n is: $formula");
+// }
+
+// int formulaFactorial(int n) {
+//   if (n == 0 || n == 1) {
+//     return 1;
+//   } else {
+//     return n * formulaFactorial(n - 1); // n! = n * (n-1)!
+//   }
+// }
